e9956912603f58566cd72e3d12fa6a6fa6ed9d16
Malindu Shamalka <mshamalka2@gmail.com>
2021-04-03 17:49:26 +0530

    index router set

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,13 +1,35 @@
 var express = require('express');
-var router = express.Router();
+var indexRouter = express.Router();
 const database = require('../database')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.send({
-    name:"Malindu"
-  })
+/*  data send to the home page for view */
+indexRouter.get('/',(req, res)=> {
+  try{
+        database.query("select * from test",(err,row)=>{
+          if(err){
+            throw err;
+          }
+          else{
+            if(row.length == 0){
+              res.end({
+                response: false,
+                data:"Some data base issue total number of items is empty :("
+              })
+            }
+            else{
+              res.send({
+                response:true,
+                //send resent items to front end home page
+                data: row
+              })
+            }
+          }
+        })
+  }
+  catch(error){
+    console.log(error);
+    throw error;
+  }
 });
 
-module.exports = router;
+module.exports = indexRouter;
